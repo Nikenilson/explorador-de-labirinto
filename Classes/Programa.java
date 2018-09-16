2565afdca244e476231f65852d087ea7707e6536
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2018-09-15 22:49:57 -0300

    2- Finalizaçao da leitura de dados, começo da busca
    Terminei a leitura dos dados e colocação na matriz, e ja fiz o codigo que acha a entrada

--- Classes/Programa.java
+++ Classes/Programa.java
@@ -1,8 +1,11 @@
-import java.io;
-publib class Programa
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+public class Programa
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args)  throws Exception
 	{
 		char[][] labirinto;
 		Pilha<Cordenada> caminho;
@@ -10,42 +13,93 @@ publib class Programa
 
 		try
 		{
-
+			Scanner ler = new Scanner(System.in);
+			System.out.printf("Informe o nome de arquivo texto:\n");
+    		String nome = ler.nextLine();
+    		int qtdLinhas;
+    		int qtdColunas;
 			try
 			{
-		      FileReader arq = new FileReader(nome);
-		      BufferedReader lerArq = new BufferedReader(arq);
+		    	FileReader arq = new FileReader(nome);
+		      	BufferedReader lerArq = new BufferedReader(arq);
 
-		      int qtdLinhas = lerArq.readLine();
-		      int qtdColunas = lerArq.readLine();
+		      	qtdLinhas = lerArq.readLine();
+		      	qtdColunas = lerArq.readLine();
 
-		      labirinto = new char[qtdColunas][qtdLinhas];
+		      	labirinto = new char[qtdColunas][qtdLinhas];
 
-			  String linha;
+			  	String linha;
 
-			  for(int e = 0; e < qtdColunas; e++)
-			  {
-				linha = lerArq.readLine();
-		      	for(int i = 0; i < qtdLinhas; i++)
-		      	labirinto[i][e] linha.charAt(i);
-		      }
+			  	for(int e = 0; e < qtdColunas; e++)
+			  	{
+					linha = lerArq.readLine();
+		      		for(int i = 0; i < qtdLinhas; i++)
+		      		labirinto[i][e] = linha.charAt(i);
+		      	}
 
-		      arq.close();
+		      	arq.close();
 		    }
-		    catch (IOException e) {
+		    catch (IOException e)
+		    {
 		        System.err.printf("Erro na abertura do arquivo: %s.\n",
 		          e.getMessage());
 		    }
+			int qtdDeCoordenadas = qtdLinhas * qtdColunas;
+		    caminho = new Pilha<Cordenada>(qtdDeCoordenadas);
 
-		    System.out.println();
-  }
+		    possibilidades = new Pilha<Fila<Coordenada>>(qtdDeCoordenadas);
+
+		    Coordenada atual = acharEntrada();
 
 
 		}
-		catch(){}
+
+
+		catch(Exception e)
+		{}
 
 
 	}
 
+	public Coordenada acharEntrada() throws Exception
+	{
+		boolean achou = false;
+		for(int X; X < qtdColunas; X++)
+		    if(labirinto[X][0] == "E")
+		    {
+		    	achou = true;
+		    	atual = new Coordenada(X,0);
+		    	break;
+			}
+		if(!achou)
+		for(int X; X < qtdColunas; X++)
+			if(labirinto[X][qtdLinhas - 1] == "E")
+			{
+				achou = true;
+				atual = new Coordenada(X,qtdLinhas - 1);
+				break;
+			}
+		if(!achou)
+		for(int Y = 1; Y < qtdLinhas - 1; Y++) //Começa em 1 e termina em qtdLinhas - 1 pois o primeiro e o ultimo ja foram verificados
+			if(labirinto[0][Y] == "E")
+			{
+				achou = true;
+				atual = new Coordenada(0,Y);
+				break;
+			}
+		if(!achou)
+		for(int Y = 1; Y < qtdLinhas - 1; Y++) //Começa em 1 e termina em qtdLinhas - 1 pois o primeiro e o ultimo ja foram verificados
+			if(labirinto[qtdColunas - 1][Y] == "E")
+			{
+				achou = true;
+				atual = new Coordenada(qtdColunas - 1,Y);
+				break;
+			}
 
+		if(achou)
+		return atual;
+
+		else
+			throw new Exception("Labirinto sem entrada valida!");
+	}
 }
