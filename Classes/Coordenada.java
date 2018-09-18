732494c1830661b895d3df4deb72b80548c1679d
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2018-09-18 11:54:45 -0300

    Classe coordenada finalizada

--- Classes/Coordenada.java
+++ Classes/Coordenada.java
@@ -1,13 +1,64 @@
 public class Coordenada
 {
-	int x;
-	int y;
+	private int x;
+	private int y;
 
 	public Coordenada(int eixoX, eixoY) throws Exception
 	{
-		x = eixoX;
-		y = eixoY;
+		if(eixoX < 0 || eixoY < 0)
+			throw new Exception("Coordenadas invalidas");
+
+		this.x = eixoX;
+		this.y = eixoY;
 	}
 
+	public int getX()
+	{
+		return this.x;
+	}
 
+	public int getY()
+	{
+		return this.y;
+	}
+
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+		return true;
+
+		if(obj == null)
+		return false;
+
+		if(obj.getClass() != this.getClass())
+		return false;
+
+		Coordenada c = (Cordenada) obj;
+
+		if(this.x != obj.x)
+		return false;
+
+		if(this.y != obj.y)
+		return false;
+
+		return true;
+	}
+
+	public String toString() //Formata o conteúdo em String
+	{
+		String oX = (x<10?"0"+x: x + "");
+		String oY = (y<10?"0"+y: y + "");
+		return "("+ oX +"," + this.y + ")";
+	}
+
+	public int hashCode()
+	{
+		int ret = 666;
+
+		ret = ret * 2 + new Integer(this.x).hashCode();
+		ret = ret * 2 + new Integer(this.y).hashCode();
+
+		return ret;
+	}
 }
+
